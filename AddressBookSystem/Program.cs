b68e147fd37b0f578fa03427b3c6e678b80d09b1
navabhi22 <navabhi22@gmail.com>
2022-03-12 22:23:02 +0530

    Greeting message for AddressBook Program

---
+++ AddressBookSystem/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program\n");
+
+            AddressBook addressBook = new AddressBook();
+
+            addressBook.AddContact("Abhinav", "Kumar", "A-34 ", "New Delhi", "Delhi", 110023 , 9876543210 , "abc123@gmail.com");
+            addressBook.ViewContact();
+        }
+    }
+}
